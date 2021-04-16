38a97126b52f56ec1f4fe5e828ab4455356ca3a2
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2021-04-15 22:42:21 -0700

    added local storage and onload function

--- pity.js
+++ pity.js
@@ -1,24 +1,22 @@
-function add(id, num) {
-    let ele = document.getElementById(id)
-    let score = parseInt(ele.innerHTML) + parseInt(num)
-    if (id == "weap") {
-        if (79 < score) {
-            ele.innerHTML = 0
+window.onload = function() {
+    for (let id of ['char', 'weap', 'stan']) {
+        if (localStorage.getItem(id) == null) {
+            localStorage.setItem(id, 0)
         }
         else {
-            ele.innerHTML = score
-        }
-    }
-    else {
-        if (89 < score) {
-            ele.innerHTML = 0
-        }
-        else {
-            ele.innerHTML = score
+            document.getElementById(id).innerHTML = localStorage.getItem(id)
         }
     }
 }
 
+function add(id, num) {
+    let ele = document.getElementById(id)
+    let score = parseInt(ele.innerHTML) + parseInt(num)
+    ele.innerHTML = id == "weap" ? (79 < score ? 0:score):(89 < score ? 0:score)
+    localStorage.setItem(id, ele.innerHTML)
+}
+
 function reset(id) {
     document.getElementById(id).innerHTML = 0
+    localStorage.setItem(id, 0)
 }
